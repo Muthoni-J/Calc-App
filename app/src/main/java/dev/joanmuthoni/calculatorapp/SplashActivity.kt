0fb5bcd51f50f17198e43fbd872faeb8ba6d49ec
Muthoni-J <jmjoanmuthoni@gmail.com>
2022-06-16 10:25:02 +0300

    first commit

---
+++ app/src/main/java/dev/joanmuthoni/calculatorapp/SplashActivity.kt
@@ -0,0 +1,15 @@
+package dev.joanmuthoni.calculatorapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.calculatorapplication.MainActivity
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+//        intent = Intent(MainActivity::Java)
+
+//        setContentView(R.layout.activity_splash)
+    }
+}
